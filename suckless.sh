c269547e242e17fd0ade2e1c360ffa8e6be79838
Cerberus <myrmidon@protonmail.ch>
2024-08-23 03:56:29 +0200

    Create suckless.sh

---
+++ suckless.sh
@@ -0,0 +1,6 @@
+ftp -o st-0.9/patches/st-focus-20230610-68d1ad9.diff https://st.suckless.org/patches/alpha_focus_highlight/st-focus-20230610-68d1ad9.diff
+ftp -o st-0.9/patches/patch-config_mk https://raw.githubusercontent.com/openbsd/ports/master/x11/st/patches/patch-config_mk
+ftp -o st-0.9/patches/patch-config_def_h https://raw.githubusercontent.com/openbsd/ports/master/x11/st/patches/patch-config_def_h
+ftp -o st-0.9/patches/st-scrollback-0.8.5.diff https://st.suckless.org/patches/scrollback/st-scrollback-0.8.5.diff
+
+
